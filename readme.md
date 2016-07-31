d44613f24fcdba20c2289b5f313ddfa410265683
dpg5000 <dgoussev@gmail.com>
2016-07-31 01:25:19 -0700

    add readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+    # NOCKETS.IO - a collaborative effort between NODE.js and SOCKETS.io
+
+    NOCKETS examines the working relationship between the server component provided by
+    Node.js and the bidirectional (client->server, server->client), real-time event-based data communication
+    as provided by Socket.io
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 /*
     NOCKETS.IO - a collaborative effort between NODE.js and SOCKETS.io
+
+    NOCKETS examines the working relationship between the server component provided by
+    Node.js and the bidirectional (client->server, server->client) event-based communication
+    as provided by Socket.io
 */
 
 var http = require('http');
